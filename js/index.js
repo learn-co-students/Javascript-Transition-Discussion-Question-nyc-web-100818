46353c6d646cf56e9ee91f9ee9d939282424d87d
brewchetta <chett.tiller@gmail.com>
2018-11-27 10:11:52 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+const zombieLincoln = document.getElementById('save_lincoln')
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+zombieLincoln.addEventListener('click', () => {
+  saveLincoln()
+})
+
+function saveLincoln() {
+  const interval = document.getElementById('interval').value
+  let transformingDecimalThing = 1
+  opacitizeZombie(transformingDecimalThing, interval)
 }
+
+function opacitizeZombie(transformingDecimalThing, interval) {
+  var opZomb = setInterval(() => {
+    if (transformingDecimalThing <= 0) {
+      console.log('Hit 0')
+      clearInterval(opZomb)
+    } else {
+      transformingDecimalThing -= 0.01
+      document.getElementById('foreground').style.opacity = transformingDecimalThing
+      console.log('!')
+    }
+  }, (interval/100))
+}
+
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
