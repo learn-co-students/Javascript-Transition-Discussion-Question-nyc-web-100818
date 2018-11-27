a369c1d31d96cd1753a1d52ae45b5d95e7030c54
sg3957 <sg3957@nyu.edu>
2018-11-27 10:36:51 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,34 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+const saveButton = document.getElementById('save_lincoln');
+const interval = document.getElementById('interval');
+const foreground = document.getElementById('foreground');
+let opacity = parseFloat(foreground.style.opacity);
+
+saveButton.addEventListener('click', function(event){
+  let intervalValue = interval.value;
+
+  if(isNaN(intervalValue) || intervalValue <= 0) {
+      intervalValue = 10000;
+    }
+
+
+  const counter = setInterval(fadeOut, intervalValue / 100);
+
+  function fadeOut(){
+
+    if (opacity > 0){
+      opacity -= 0.01;
+    } else {
+      clearInterval(counter)
+    }
+    foreground.style.opacity = opacity;
   }
-  $('#foreground').fadeOut(interval);
-}
+});
