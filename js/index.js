d6a8fb0cb317015b2a563c57e821c0ca00a0952f
ihollander <ianholla@gmail.com>
2018-11-28 10:21:37 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+const saveButton = document.getElementById('save_lincoln')
+const interval = document.getElementById('interval')
+const foreground = document.getElementById('foreground')
+const transition = 100
+let opacity = parseFloat(foreground.style.opacity) || 1
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+saveButton.addEventListener('click', () => {
+  let intervalValue = interval.value
+  if (isNaN(intervalValue) || intervalValue <= 0) {
+    intervalValue = 10000
   }
-  $('#foreground').fadeOut(interval);
-}
+  let counter = setInterval(fadeOut, intervalValue / transition)
+
+  function fadeOut() {
+    if (opacity > 0) {
+      opacity -= (1 / transition)
+    } else {
+      clearInterval(counter)
+    }
+    foreground.style.opacity = opacity
+  }
+})
